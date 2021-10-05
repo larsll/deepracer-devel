4c56b366f0bb42f12123d28bf101c0566428e4c1
Lars Lorentz Ludvigsen <larsll@outlook.com>
2021-10-05 03:24:32 -0400

    Updated orientation

--- misc/bmi160.py
+++ misc/bmi160.py
@@ -13,19 +13,18 @@ sensor.setFullScaleGyroRange(definitions.GYRO_RANGE_250, 250.0)
 sensor.autoCalibrateXAccelOffset(0)
 sensor.autoCalibrateYAccelOffset(0)
 sensor.autoCalibrateZAccelOffset(-1)
-
 sleep(0.3)
-sensor.setAccelOffsetEnabled(False)
+sensor.setAccelOffsetEnabled(True)
 
 while True:
   data = sensor.getMotion6()
   # fetch all gyro and acclerometer values
   print({
-    'gx': '{:+.1f}'.format(data[0] / 0x8000 * 250 * (math.pi / 180)),
-    'gy': '{:+.1f}'.format(data[1] / 0x8000 * 250 * (math.pi / 180)),
-    'gz': '{:+.1f}'.format(data[2] / 0x8000 * 250 * (math.pi / 180)),
-    'ax': '{:+.1f}'.format(data[3] / 0x8000 * 4),
-    'ay': '{:+.1f}'.format(data[4] / 0x8000 * 4),
-    'az': '{:+.1f}'.format(data[5] / 0x8000 * 4)
+    'gx': '{:+.1f}'.format(data[1] / 0x8000 * 250 * (math.pi / 180)),
+    'gy': '{:+.1f}'.format(data[0] / 0x8000 * 250 * (math.pi / 180)),
+    'gz': '{:+.1f}'.format(data[2] / 0x8000 * -250 * (math.pi / 180)),
+    'ax': '{:+.1f}'.format(data[4] / 0x8000 * 4),
+    'ay': '{:+.1f}'.format(data[3] / 0x8000 * 4),
+    'az': '{:+.1f}'.format(data[5] / 0x8000 * -4) 
   })
   
