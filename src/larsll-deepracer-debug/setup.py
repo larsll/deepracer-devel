1caf1afffddab1e2d0335eb03a281080dda1e94f
Lars Ludvigsen <larsll@outlook.com>
2022-08-30 06:24:23 +0000

    Debug package

--- src/larsll-deepracer-debug/setup.py
+++ src/larsll-deepracer-debug/setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup
 
 package_name = 'debug_pkg'
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name), ["launch/debug_pkg_launch.py"]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'test = debug_pkg.debug_imu_node:main'
+            'imu = debug_pkg.debug_imu_node:main'
         ],
     },
 )
