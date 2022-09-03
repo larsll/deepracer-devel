1b771df6cbd7994b91728368a626e32e43377212
Lars Ludvigsen <larsll@outlook.com>
2022-09-03 11:49:52 +0000

    Create launch and command files.

---
+++ misc/start_inference.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ros2 service call /camera_pkg/media_state deepracer_interfaces_pkg/srv/VideoStateSrv "{activate_video: 1}"
+
+ros2 service call /inference_pkg/load_model deepracer_interfaces_pkg/srv/LoadModelSrv "{artifact_path: '/opt/aws/deepracer/artifacts/Sample_single_cam/model.xml', task_type: 0, pre_process_type: 1, action_space_type: 1}"
+
+ros2 service call /deepracer_navigation_pkg/load_action_space deepracer_interfaces_pkg/srv/LoadModelSrv "{artifact_path: '/opt/aws/deepracer/artifacts/Sample_single_cam/model_metadata.json', task_type: 0, pre_process_type: 1, action_space_type: 1}"
+
+ros2 service call /inference_pkg/inference_state deepracer_interfaces_pkg/srv/InferenceStateSrv "{start: 1, task_type: 0}"
