613a02c0afe7aef867867c2406ede512a9eb5cd6
Lars Ludvigsen <larsll@outlook.com>
2024-02-28 22:02:53 +0000

    Fix API call

--- misc/optimize_model.sh
+++ misc/optimize_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ros2 launch launch/optimize.launch
 
-ros2 service call /model_optimizer_pkg/model_optimizer_server deepracer_interfaces_pkg/srv/ModelOptimizeSrv "{Sample_single_cam/model', model_metadata_sensors: [1], training_algorithm: 1, img_format: "BGR", width: 160, height: 120, num_channels: 1, lidar_channels: 0, platform: 0}"
+ros2 service call /model_optimizer_pkg/model_optimizer_server deepracer_interfaces_pkg/srv/ModelOptimizeSrv "{model_name: 'Sample_single_cam/model', model_metadata_sensors: [1], training_algorithm: 1, img_format: "BGR", width: 160, height: 120, num_channels: 1, lidar_channels: 0, platform: 0}"
