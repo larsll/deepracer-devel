fc5756cb81336db7b2d8ae4490b754b13b9a83b3
Lars Ludvigsen <larsll@outlook.com>
2024-03-17 16:42:45 +0000

    Logging evolution

--- misc/start_inference.sh
+++ misc/start_inference.sh
@@ -5,4 +5,4 @@ ros2 service call /deepracer_navigation_pkg/load_action_space deepracer_interfac
 
 ros2 service call /camera_pkg/media_state deepracer_interfaces_pkg/srv/VideoStateSrv "{activate_video: 1}"
 
-ros2 service call /inference_pkg/inference_state deepracer_interfaces_pkg/srv/InferenceStateSrv "{start: 1, task_type: 0}"
+# ros2 service call /inference_pkg/inference_state deepracer_interfaces_pkg/srv/InferenceStateSrv "{start: 1, task_type: 0}"
